d402e18a35b5ffd713107728364ba9492f8ffe85
sergionet23 <sergionet23@gmail.com>
2021-04-07 23:07:00 -0300

    Ubdate icons

--- app/src/main/java/com/acharcitox/telocuido/RegisterActivity.java
+++ app/src/main/java/com/acharcitox/telocuido/RegisterActivity.java
@@ -17,7 +17,7 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        // Initialice Firebase connections 
+        // Initialize Firebase connections
         FirebaseApp.initializeApp(this);
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = firebaseDatabase.getReference();
